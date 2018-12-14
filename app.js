53acb6bb768efcdb14d7019a2fb2770f4a3f40a8
Anuj Bastola <abastol1@ramapo.edu>
2018-12-14 10:01:18 -0500

    Table Search Done

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var MapboxClient = require('mapbox');
+var geo = require('mapbox-geocoding');
+
+geo.setAccessToken('pk.eyJ1IjoiYWJhc3RvbDEiLCJhIjoiY2pvc3htNnZmMHJzNTNvczBqYWRnd2FqZyJ9.2tD5ZaeSDK46QnnjMnNCTw');
 
 var app = express();
 app.use(bodyParser.json());
@@ -11,46 +15,31 @@ app.use(express.static('views'));
 
 
 var Contact = mongoose.model('Contact', {
+    prefix: String,
     firstName: String,
     lastName: String,
     street: String,
+    city: String,
     state: String,
     zip: String,
     phone: String,
     email: String,
-    prefix: String,
-    contactByMail: Boolean,
-    contactByPhone: Boolean,
-    contactByEmail: String
+    contactByMail: String,
+    contactByPhone: String,
+    contactByEmail: String,
+    lat: String,
+    lng: String
 });
-
-var document = {
-    firstName: "Anuj",
-    secondName: "Bastola",
-    street: "505 Ramapo Valley Road",
-    state: "New Jersey",
-    zip: "07430",
-    phone: "5512629132",
-    email: "abastol1@ramapo.edu",
-    prefix: "Mr",
-    contactByMail: true,
-    contactByPhone: false,
-    contactByEmail: "abastol1@ramapo.edu"
-}
-
-Contact.insertMany(document);
 
 mongoose.connect('mongodb://webfinal:webfinal1@ds015942.mlab.com:15942/webappfinal', { useNewUrlParser: true }, (err) => {
     console.log('Mongodb Connected', err);
 })
 
-
 app.get('/mailer', function (req, res) {
-    res.sendFile(__dirname + '/views/home/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 })
 
-
-app.post('mailer', urlencodedParser, (req, res) => {
+app.post('/mailer', urlencodedParser, (req, res) => {
     let mailBool = "No";
     let isPhone = "No";
     let isEmail = "No";
@@ -65,24 +54,128 @@ app.post('mailer', urlencodedParser, (req, res) => {
         isEmail = req.body.emailAdd;
     }
 
-
-    var document = {
-        firstName: req.body.firstName,
-        secondName: req.body.lastName,
-        street: req.body.body.street,
-        city: req.body.city,
-        state: req.body.state,
-        zip: req.body.zip,
-        phone: req.body.phone,
-        email: req.body.email,
-        prefix: req.body.prefix,
-        contactByMail: mailBool,
-        contactByPhone: isPhone,
-        contactByEmail: isEmail
+    if (isPhone == "Yes") {
+        isPhone = req.body.phoneNum;
     }
 
+    const addressToGeoCode = req.body.street + ", " + req.body.city + ", " + req.body.state + " " + req.body.zip;
+    console.log("Address to geocode is " + addressToGeoCode);
 
+    geo.geocode('mapbox.places', addressToGeoCode, function (err, geoData) {
+        const lng = geoData.features[0].center[0];
+        const lat = geoData.features[0].center[1];
+        console.log("Longitude is " + lng);
+        console.log("Latitude  is " + lat);
+
+        var document = {
+            prefix: req.body.prefix,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            street: req.body.street,
+            city: req.body.city,
+            state: req.body.state,
+            zip: req.body.zip,
+            phone: req.body.phoneNum,
+            email: req.body.emailAdd,
+            contactByMail: mailBool,
+            contactByPhone: isPhone,
+            contactByEmail: isEmail,
+            lat: lat,
+            lng: lng
+        }
+
+        var toSend = {
+            fullName: document.prefix + " " + document.firstName + " " + document.lastName,
+            address: document.street + ", " + document.city + ", " + document.state + " " + document.zip,
+            contactByPhone: isPhone,
+            contactByMail: mailBool,
+            contactByEmail: isEmail,
+            lng: lng,
+            lat: lat
+        }
+
+        console.log("The document is " + JSON.stringify(document));
+        Contact.insertMany(document);
+        res.render('mapPage/mapContactPage.pug', { toSend: toSend });
+
+    });
 })
 
+app.get("/contacts", urlencodedParser, (req, res) => {
+    Contact.find({}, (err, data) => {
+        console.log("lenngth is " + data.length);
+        console.log("Each is " + JSON.stringify(data[0]));
+        res.render('contacts.pug', { data: data });
+        // res.send(data);
+    });
+})
+
+
+app.post('/update', urlencodedParser, function (req, res) {
+    var newData = req.body;
+    console.log("Inside post method");
+    console.log("Firstname in post update is " + newData.firstName);
+    console.log("New Data is " + JSON.stringify(newData));
+    var fullAddress = newData.street + " " + newData.city + " " + newData.state + " " + newData.zip;
+    console.log("Fulll address is " + fullAddress);
+    let mailBool = "No";
+    let isPhone = "No";
+    let isEmail = "No";
+
+    if (req.body.mail == "Mail" || req.body.any == "Any") {
+        mailBool = "Yes";
+    }
+    if (req.body.phone == "Phone" || req.body.any == "Any") {
+        isPhone = "Yes";
+    }
+    if (req.body.email == "Email" || req.body.any == "Any") {
+        isEmail = req.body.emailAdd;
+    }
+
+    if (isPhone) {
+        isPhone = req.body.phoneNum;
+    }
+
+    geo.geocode('mapbox.places', fullAddress, function (err, geoData) {
+        const lng = geoData.features[0].center[0];
+        const lat = geoData.features[0].center[1];
+        // console.log("Longitude is " + lng);
+        // console.log("Latitude  is " + lat);
+        newData.lat = lat;
+        newData.lng = lng;
+        console.log("Object is " + JSON.stringify(newData));
+
+        Contact.updateOne(
+            { "_id": newData.custId },
+            { $set: { "prefix": newData.prefix, "firstName": newData.firstName, "lastName": newData.lastName, "street": newData.street, "city": newData.city, "state": newData.state, "zip": newData.zip, "phone": newData.phoneNum, "email": newData.emailAdd, "lat": newData.lat, "lng": newData.lng, "contactByMail": mailBool, "contactByPhone":isPhone, "contactByEmail":isEmail } }
+        )
+            .then((obj) => {
+                console.log("Updated - " + JSON.stringify(obj));
+                Contact.find({}, (err, data) => {
+                    console.log("lenngth is " + data.length);
+                    console.log("All data after alert is  " + JSON.stringify(data));
+                    // res.send(data);
+                    res.render('contacts.pug', { data: data });
+                });
+            })
+    });
+})
+
+app.post('/delete', function (req, res) {
+    console.log("Inside delete post method in server");
+    console.log("Inside delete is " + req.body.contactId);
+
+    var query = Contact.remove({ _id: req.body.id });
+
+    Contact.deleteOne({ "_id": req.body.contactId }, function (err) {
+        if (err) {
+            throw err;
+        }
+        else {
+            console.log("Deleted!!!!!!!!!!!!!");
+            res.send("Success");
+        }
+    });
+})
 
 app.listen(3000);
---
+++ views/geocoding.js
@@ -0,0 +1,151 @@
+
+$(document).ready(function () {
+    console.log("I am inside Geocoding.js");
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWJhc3RvbDEiLCJhIjoiY2pvc3htNnZmMHJzNTNvczBqYWRnd2FqZyJ9.2tD5ZaeSDK46QnnjMnNCTw';
+
+    $('#updateForm').hide();
+
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [-74.003807, 40.753250],
+        zoom: 13
+    });
+
+    var marker = new mapboxgl.Marker()
+        .setLngLat([-74.003807, 40.753250])
+        .addTo(map);
+
+
+    // Create a popup, but don't add it to the map yet.
+    var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
+    // -------------------------------------------------------------------------------------------------------------------------------
+    // adding marker 
+
+    $('table > tbody > tr').each(function () {
+        console.log("--------------------------------------");
+        var lat = $(this).data("lat");
+        var lng = $(this).data("lng");
+
+        console.log("Lat is " + lat);
+        console.log("Lng is " + lng);
+
+        var marker = new mapboxgl.Marker()
+            .setLngLat([lng, lat])
+            .addTo(map);
+    });
+
+    // -------------------------------------------------------------------------------------------------------------------------------
+    // When user clicks on any row in the table, this function is called to center the map to that particular location
+    $(".clickable").on("click", "tr", function () {
+        var name = $(this).data("name");
+        var prefix = $(this).data("prefix");
+        var address = $(this).data("address");
+        var lat = $(this).data("lat");
+        var lng = $(this).data("lng");
+        var phone = $(this).data("phone");
+        var email = $(this).data("email");
+        console.log("Name is " + name);
+        console.log("Prefix is " + prefix)
+        if(typeof prefix == "undefined"){
+            console.log("UNDEFINED PREFIX")
+            // DO not add prefix to name
+        }
+        else {
+            // add prefix to name
+            name = prefix + " " + name;
+        }
+
+        var description = "<h6>" + name + "</h6>" + "<h6>" + address + "</h6>" + "<h6>" + phone + "</h6>" + "<h6>" + email + "</h6>";
+        var popup = new mapboxgl.Popup({ closeOnClick: false })
+            .setLngLat([lng, lat])
+            .setHTML(description)
+            .addTo(map);
+
+        map.flyTo({
+            center: [lng, lat]
+        });
+    })
+
+    // -------------------------------------------------------------------------------------------------------------------------------
+    $(".clickable-row").on("click", "#updateButton", function () {
+        console.log("Button is clicked");
+        $("#container").hide();
+        $("#map").hide();
+        $('form').show();
+
+        var prefix = $(this).data("prefix");
+        var firstName = $(this).data("firstname");
+        var lastName = $(this).data("lastname");
+        var street = $(this).data("street");
+        var city = $(this).data("city");
+        var state = $(this).data("state");
+        var zip = $(this).data("zip");
+        var email = $(this).data("email");
+        var phone = $(this).data("phone");
+        console.log("id is " + $(this).data("id"));
+        if (prefix == "Mr.") {
+            console.log("Inside mr.");
+            $("#radio1").prop("checked", true);
+        }
+        else if (prefix == "Mrs.") {
+            $("#radio2").prop("checked", true);
+        }
+        else if (prefix == "Ms.") {
+            $("#radio3").prop("checked", true);
+        }
+        else {
+            $("#radio4").prop("checked", true);
+        }
+
+        $("#inputFirstName4").val(firstName);
+        $("#inputFirstName4").attr('data-id', $(this).data("id"));
+        $("#custId").val($(this).data("id"));
+
+        $("#inputLastName4").val(lastName);
+        $("#inputAddress").val(street);
+        $("#inputAddress2").val(city);
+        $("#inputState").val(state).attr("selected", "selected");
+        $("#inputZip").val(zip);
+        $("#inputPhone").val(phone);
+        $("#inputEmail").val(email);
+        
+    })
+
+    $(".clickable-row").on("click", "#deleteButton", function () {
+        console.log("Inside Delete button");
+        var id = { contactId: $(this).data('id') };
+        console.log("Delete id is " + id);
+
+        $(this).parent().parent().remove();
+        
+        $.ajax({
+            type: 'POST',
+            data: JSON.stringify(id),
+            contentType: 'application/json',
+            url: 'http://localhost:3000/delete',
+            success: function (data) {
+                alert("Contact Deleted ");
+            },
+            error: function (err) {
+                alert("ERROR!")
+            }
+        });
+    });
+
+    $("#searchTable").on("keyup", function() {
+        console.log("Inside filter");
+        var value = $(this).val().toLowerCase();
+        $("#tableBody tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+});
+
+
+
+
